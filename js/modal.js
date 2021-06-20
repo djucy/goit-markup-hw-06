dedec5988b9dc9c144bed221b79ead25a2186e6d
Yevheniia Dolzhykova <nikola.tesla0510@gmail.com>
2021-06-20 14:54:52 +0300

    add

--- js/modal.js
+++ js/modal.js
@@ -14,13 +14,3 @@
 })();
 
 
-
-(() => {
-document.querySelector('.js-speaker-form').addEventListener('submit', e => {
-e.preventDefault();
-
-new FormData(e.currentTarget).forEach((value, name) =>
-console.log(`${name}: ${value}`),
-);
-});
-})();
